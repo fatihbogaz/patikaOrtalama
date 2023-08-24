73ebdb0f3aaefa946fd8bdd387285005ea13d455
Fatih <fatihbogaz@gmail.com>
2023-08-24 12:51:44 +0300

    s

--- src/main/java/giris/Main.java
+++ src/main/java/giris/Main.java
@@ -1,21 +1,45 @@
-package giris
+package giris;
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        byte kisaKenar=10, uzunKenar=7;
-        int cevre= 2*(kisaKenar+uzunKenar);
-        int alan= kisaKenar*uzunKenar;
-        String alanAdi="Alan:";
-        String cevreAdi="Cevre:";
-        System.out.println(cevreAdi+cevre);
-        System.out.println(alanAdi+alan);
+
+        int mat, fizik, kimya, turkce, tarih, muzik;
+
+       Scanner input=new Scanner(System.in);
+        System.out.print("Matamatik dersinin notunu giriniz:");
+        mat=input.nextInt();
+
+        System.out.print("Fizik dersinin notunu giriniz:");
+        fizik=input.nextInt();
+
+        System.out.print("Kimya dersinin notunu giriniz:");
+        kimya=input.nextInt();
+
+        System.out.print("Türkçe dersinin notunu giriniz:");
+        turkce=input.nextInt();
+
+        System.out.print("Tarih dersinin notunu giriniz:");
+        tarih =input.nextInt();
+
+        System.out.print("Müzik dersinin notunu giriniz:");
+        muzik=input.nextInt();
+
+        double sonuc= (mat+fizik+kimya+turkce+tarih+muzik)/6.0;
+
+        System.out.println("Not Ortalaması:"+sonuc);
+
+        boolean gecti= sonuc>60;
+        String nihai= gecti ? "Not ortalamanız 60'tan yüksek. Tebrikler Sınıfı Geçtiniz" : "Not ortalamanız 60'tan düşük. Sınıfta kaldınız.";
+
+        System.out.println(nihai);
 
 
 
-        /**byte a= 120;
-        short b= 200;
-        int c = 700;
-        long t = 1000000000;
-        String A = "selamlar:";
-        System.out.println(A+a);*/
+
+
+
+
+
+
     }
-}
+    }
