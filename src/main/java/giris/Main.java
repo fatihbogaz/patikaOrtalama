6cc752d73fdabbc297e6862af9395870805683d9
Fatih <fatihbogaz@gmail.com>
2023-08-24 12:40:05 +0300

    first commit

---
+++ src/main/java/giris/Main.java
@@ -0,0 +1,21 @@
+package giris
+public class Main {
+    public static void main(String[] args) {
+        byte kisaKenar=10, uzunKenar=7;
+        int cevre= 2*(kisaKenar+uzunKenar);
+        int alan= kisaKenar*uzunKenar;
+        String alanAdi="Alan:";
+        String cevreAdi="Cevre:";
+        System.out.println(cevreAdi+cevre);
+        System.out.println(alanAdi+alan);
+
+
+
+        /**byte a= 120;
+        short b= 200;
+        int c = 700;
+        long t = 1000000000;
+        String A = "selamlar:";
+        System.out.println(A+a);*/
+    }
+}
